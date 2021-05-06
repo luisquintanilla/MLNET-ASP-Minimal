6a2e7354e6ffcc739b92d83e59240035408afcb0
luisquintanilla <Luis.Quintanilla@microsoft.com>
2021-05-06 11:00:28 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Configure app
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddPredictionEnginePool<ModelInput,ModelOutput>()
+    .FromUri("https://github.com/dotnet/samples/raw/main/machine-learning/models/sentimentanalysis/sentiment_model.zip");
+
+var app = builder.Build();
+
+// Define prediction route & handler
+app.MapPost("/predict", 
+    async ([FromServices] PredictionEnginePool<ModelInput,ModelOutput> predictionEnginePool, ModelInput input) => 
+        await Task.FromResult(predictionEnginePool.Predict(input)));
+
+// Run app
+app.Run();
